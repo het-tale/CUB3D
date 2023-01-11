e9829516946045c60411b13d8cbb38f5a2894aa3
Aya Heddak <aheddak@e2r10p2.1337.ma>
2023-01-11 21:58:59 +0100

    finished parsing

--- parser/parser/textures.c
+++ parser/parser/textures.c
@@ -6,7 +6,7 @@
 /*   By: aheddak <aheddak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 08:18:18 by aheddak           #+#    #+#             */
-/*   Updated: 2023/01/11 21:34:53 by aheddak          ###   ########.fr       */
+/*   Updated: 2023/01/11 21:58:20 by aheddak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,30 @@ int	get_line_info(char *str, char *s2, int size, char **tex)
 	return (0);
 }
 
+int	check_for_textures(char *str, t_mlx *param)
+{
+	if (get_line_info(str, "NO ", 3, &param->txt[0].name) == 1)
+		return (1);
+	else if (get_line_info(str, "SO ", 3, &param->txt[1].name) == 1)
+		return (1);
+	else if (get_line_info(str, "WE ", 3, &param->txt[3].name) == 1)
+		return (1);
+	else if (get_line_info(str, "EA ", 3, &param->txt[2].name) == 1)
+		return (1);
+	return (0);
+}
+
+int	check_for_colors(char *str, t_mlx *param)
+{
+	if (get_line_info(str, "F ", 2, &param->colors->f) == 1)
+		return (1);
+	else if (get_line_info(str, "C ", 2, &param->colors->c) == 1)
+		return (1);
+	else if (*str == '\n')
+		return (1);
+	return (0);
+}
+
 int	treat_textures(char **map, t_mlx *param, int i)
 {
 	char	*str;
@@ -42,37 +66,7 @@ int	treat_textures(char **map, t_mlx *param, int i)
 	{
 		free(str);
 		str = skip_spaces(map[i]);
-		if (get_line_info(str, "NO ", 3, &param->txt[0].name) == 1)
-		{
-			i++;
-			continue ;
-		}
-		else if (get_line_info(str, "SO ", 3, &param->txt[1].name) == 1)
-		{
-			i++;
-			continue ;
-		}
-		else if (get_line_info(str, "WE ", 3, &param->txt[3].name) == 1)
-		{
-			i++;
-			continue ;
-		}
-		else if (get_line_info(str, "EA ", 3, &param->txt[2].name) == 1)
-		{
-			i++;
-			continue ;
-		}
-		else if (get_line_info(str, "F ", 2, &param->colors->f) == 1)
-		{
-			i++;
-			continue ;
-		}
-		else if (get_line_info(str, "C ", 2, &param->colors->c) == 1)
-		{
-			i++;
-			continue ;
-		}
-		else if (*str == '\n')
+		if (check_for_textures(str, param) || check_for_colors(str, param))
 		{
 			i++;
 			continue ;
